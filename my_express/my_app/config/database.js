340b0f3ee438ea2f552ab72c9f763c228390adcd
Juan Carlos Cardenas Viera <jcc@Juans-MacBook-Pro.local>
2017-10-23 21:57:46 -0500

    API: beers

---
+++ my_express/my_app/config/database.js
@@ -0,0 +1,30 @@
+{
+  "development": {
+    "username": "jcc",
+    "password": "Passw0rd",
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "jcc",
+    "password": "Passw0rd",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "jcc",
+    "password": "Passw0rd",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "staging": {
+    "username": "jcc",
+    "password": "Passw0rd",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
